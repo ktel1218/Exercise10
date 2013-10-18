58cb8ca3198f928b67f9b305de3060640fb008b7
ktel1218 <ktel1218@gmail.com>
2013-10-18 16:28:03 -0700

    task 2 finished

--- Exercise10/task2.py
+++ Exercise10/task2.py
@@ -9,3 +9,12 @@ c, 4
 d, 6
 """
 d = {"q": 5, "m": 3, "z":2, "a": 10}
+
+keys = []
+for key in d:
+    keys.append(key)
+
+sorted_keys = sorted(keys)
+
+for item in sorted_keys:
+    print "%s, %r"%(item, d[item])
